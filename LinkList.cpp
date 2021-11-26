2e6f8e4f79d9faac18b8a6a77ec2554aa2f9b96b
Prashant <prashant200025@gmail.com>
2021-11-26 14:03:28 +0530

    Modified LinkList

--- LinkList.cpp
+++ LinkList.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 #include <stdlib.h>
 
 using namespace std;
@@ -13,13 +12,15 @@ class Linklist
 {
     public:
         Node* head;
-        Node* temp;
+        Node* tail;
+        Node* temp2;
+        Node* prev;
         Linklist(){
             head = NULL;
         }
 
         void insert(int val){
-            Node* new_node = new Node();
+            Node* new_node = new Node;
             new_node->data = val;
             new_node->next = NULL;
             if(head == NULL){
@@ -27,54 +28,94 @@ class Linklist
                 cout<<"Head Inserted\n\n";
             }
             else{
-                temp->next = new_node;
+                tail->next = new_node;
                 cout<<"Node Inserted\n\n"; 
             }
-            temp = new_node;
+            tail = new_node;
         }
 
-        void remove(int val){                       //Head can't be removed
-            Node* it = head;               
-            while(it->next !=NULL){
-                if(it->data == val){
-                    temp->next = it->next;
-                    cout<<"\nElement removed\n";
+        void remove(int val){                       
+            Node* it = head; 
+            bool flag = false;  
+
+
+            if(head != NULL){
+                if(head->data == val){
+                    head = head->next;
+                    cout<<"Head Removed\n\n";
+                }     
+                else{      
+                    while(it !=NULL){
+                        if(it->data == val){
+                            if(it->next == NULL){
+                                prev->next = NULL;
+                                tail = prev;
+                                flag = true;
+                                break;
+                            }
+                            prev->next = it->next;
+                            // delete it;
+                            flag = true;
+                            cout<<"Element removed\n\n";
+                            break;
+                        }
+                        prev = it;
+                        it = it->next; 
+                    } 
+                    if(!flag){
+                        cout<<"\nElement not found!\n";
+                    }  
                 }
-                temp = it;
-                it = it->next;
             }
-
+            else{
+                cout<<"Underflow!\n\n";
+            }
         }
 
-        void print(){                               //Condition for empty Linklist
+        void print(){                               
             Node* it = head;
-            cout<<"\nNode Elements are : "<<endl;
-            while(it->next !=NULL){
-                cout<<it->data<<"->";
-                it = it->next;
+            if(head == NULL){ 
+               cout<<"List is empty!\n"<<endl; 
             }
-            cout<<it->data<<"\n\n";
+            else{
+                cout<<"\nNode Elements are : "<<endl;
+                while(it->next !=NULL){
+                    cout<<it->data<<"->";
+                    it = it->next;
+                }
+                cout<<it->data<<"\n\n";
+            }
+        }
+
+        bool search(int val){
+            Node* it = head; 
+            bool flag = false;  
+            while(it !=NULL){
+                if(it->data == val){
+                    flag = true;
+                    return flag;
+                }
+                it = it->next; 
+            }
+            return flag;
         }
 };
 
 int main(){
     Linklist list;
     int ch,x;
-
-    cout<<"Link List \n1. Insert\n2. Delete\n3. Print\n4. Exit\n";
-    while (true)
-    {
+    cout<<"Link List \n1. Insert\n2. Delete\n3. Print\n4. Search\n5. Exitn";
+    while (true){
         cout<<"Enter Choice : ";
         cin>>ch;
-        switch (ch)
-        {
+        switch (ch){
         case 1:
-            cout<<"\nEnter the element : ";
+            cout<<"Insert : ";
             cin>>x;
             list.insert(x);
             break;
         case 2:
-            cout<<"\nEnter the element : ";
+            cout<<"Remove : ";
             cin>>x;
             list.remove(x);
             break;
@@ -82,8 +123,20 @@ int main(){
             list.print();
             break;
         case 4:
+            cout<<"Search : ";
+            cin>>x;
+            if(list.search(x)){
+                cout<<"Element found!\n\n";
+            }
+            else{
+                cout<<"Element not found!\n\n";
+            }
+            break;
+        case 5:
             exit(0);
             break;
+
+        
         default:
             cout<<"Enter correct choice!";
             break;
